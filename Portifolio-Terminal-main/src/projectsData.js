d12b727cc55c28a75646db9b910df5aa2d9db0e4
Arthur Pedra <118223096+arthies2323@users.noreply.github.com>
2025-08-29 20:11:03 -0300

    Adição do 2 projeto

--- Portifolio-Terminal-main/src/projectsData.js
+++ Portifolio-Terminal-main/src/projectsData.js
@@ -8,5 +8,13 @@ export const projectsData = [
     image: '/paginicialsga.png',
     repoLink: 'https://github.com/ICEI-PUC-Minas-PMGES-TI/pmg-es-2024-2-ti2-3740100-sga-auto-escola',
     technologies: ['JavaScript', 'HTML' , 'CSS']
+  },
+   {
+    id: 2,
+    title: 'Santo Restauro',
+    description: '[EM CONSTRUÇÃO] Sistema para organização de acervos de museus, feito na disciplina de Trabalho Interdisciplinar 4 ',
+    image: '/Captura de tela 2025-08-29 200417.png',
+    repoLink: 'https://github.com/ICEI-PUC-Minas-PMGES-TI/pmg-es-2024-2-ti2-3740100-sga-auto-escola',
+    technologies: ['Django', 'Postgresql' , 'React']
   }
 ];
